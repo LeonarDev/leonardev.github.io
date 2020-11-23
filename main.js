8ca87efbc4ab8853ad67e9e0d3465f35e83043fd
LeoMajevski <leonardo.majevski@gmail.com>
2020-11-23 13:58:51 -0300

    add animaçao no .menu-itens

--- main.js
+++ main.js
@@ -6,4 +6,13 @@ const navLinks = document.querySelectorAll(".nav-link");
 selectElement(".burguer-menu-icon").addEventListener("click", ()=>{
   selectElement(".nav-list").classList.toggle("active");
   selectElement(".burguer-menu-icon").classList.toggle("toggle")
+
+  navLinks.forEach((link,index) => {
+    if (link.style.animation){
+      link.style.animation = ""
+    }else{
+      link.style.animation = `navLinkAnimated 0.5s ease forwards ${ index/7 + 0.5}s`
+
+    }
+  })
 });
