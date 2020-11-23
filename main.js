46e4a0ee084a910cc00ecbbb45f7a7677c504ed5
LeoMajevski <leonardo.majevski@gmail.com>
2020-11-23 20:12:16 -0300

    add sessao about, services e projects

--- main.js
+++ main.js
@@ -15,3 +15,18 @@ selectElement(".burguer-menu-icon").addEventListener("click", ()=>{
     }
   })
 });
+
+navLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    selectElement(".nav-list").classList.toggle("active");
+    selectElement(".burguer-menu-icon").classList.toggle("toggle");
+
+    navLinks.forEach((link,index) => {
+      if (link.style.animation){
+        link.style.animation = ""
+      }else{
+        link.style.animation = `navLinkAnimated 0.5s ease forwards ${ index/7 + 0.5}s`
+      }
+    })
+  })
+})
