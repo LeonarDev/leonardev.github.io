75d4135021ce14c5ac1c4c84507377f89241206e
Leonardo Majevski <74429273+LeonarDev@users.noreply.github.com>
2021-05-27 20:37:07 -0300

    Update script.js
    Refatora todo o código

--- projects/to-do-list/script.js
+++ projects/to-do-list/script.js
@@ -1,82 +1,226 @@
-const textTask = document.querySelector('#texto-tarefa');
-const buttonCreate = document.querySelector('#criar-tarefa');
-const taskList = document.querySelector('#lista-tarefas');
-const buttonRemoveSelected = document.querySelector('#remover-selecionado');
-const buttonRemoveAll = document.querySelector('#apaga-tudo');
-const buttonRemoveAllCompleted = document.querySelector('#remover-finalizados');
+/* Fontes:
+https://developer.mozilla.org/
+https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_node_removechild_while
+*/
+// 5 | 10 | 11 | 12 | 14- Função para criar botão
+function criarBotao(descricao, identificador, pai) {
+  const elementoPai = document.getElementById(pai);
+  const botao = document.createElement('button');
+  botao.innerText = descricao;
+  botao.id = identificador;
+  botao.classList.add('botao', identificador);
+  elementoPai.appendChild(botao);
+}
 
-// Button to create a new li
-buttonCreate.addEventListener('click', function () {
-  if (textTask.value !== '') {
-    const addTask = document.createElement('li');
-    addTask.innerText = textTask.value;
-    addTask.style.marginTop = '3px';
-    taskList.appendChild(addTask);
-    textTask.value = '';
-  }
-});
+// 5 | 6- Adiciona uma li à lista ordenada com o valor digitado pelo usuário
+function criarTarefa() {
+  const entradaTextoTarefa = document.getElementById('texto-tarefa');
+  const listaTarefas = document.getElementById('lista-tarefas');
+  const novaTarefa = document.createElement('li');
+  novaTarefa.className = 'tarefa';
+  novaTarefa.innerText = entradaTextoTarefa.value;
+  listaTarefas.appendChild(novaTarefa);
+  entradaTextoTarefa.value = '';
+}
 
-// Creates li element by pressing ENTER
-textTask.addEventListener('keyup', function(event) {
-  event.preventDefault();
-  if (event.key === 'Enter') {
-    buttonCreate.click();
-  }
-});
+// 5- Adiciona um evento ao botão criar tarefa que chama a função 'criarTarefa'
+function funcaoAdicionar() {
+  const botao = document.querySelector('#criar-tarefa');
+  botao.addEventListener('click', function () {
+    criarTarefa();
+  });
+}
 
-// Add a 'selected' class when clicking on a li - only one li can have this class
-taskList.addEventListener('click', function (itemList) {
-  const selectedItem = document.querySelector('.selected');
-  if (selectedItem != null) {
-    // selectedItem.className = '';
-    selectedItem.classList.remove('selected');
-    selectedItem.style.backgroundColor = 'rgb(255, 255, 255)';
-  }
-  if (itemList.target.className !== 'completed') {
-    itemList.target.className = 'selected';
-    itemList.target.style.backgroundColor = 'rgb(128, 128, 128)';
-  }
-});
+// Adiciona um evento 'tecla Enter' ao botão criar tarefa
+function enterAdicionar() {
+  const caixaInput = document.querySelector('#texto-tarefa');
+  caixaInput.addEventListener('keyup', function (event) {
+    if (event.keyCode === 13) {
+      criarTarefa();
+    }
+  });
+}
 
-// Add a 'completed' class and a 'checked' symbol when double-clicking on a li
-taskList.addEventListener('dblclick', function (itemList) {
-  itemList.target.style.backgroundColor = 'rgb(255, 255, 255)';
-  if (itemList.target.className === 'completed') {
-    itemList.target.className = '';
-    itemList.target.style.textDecoration = 'none';
-  } else {
-    itemList.target.className = 'completed';
-    itemList.target.style.textDecoration = 'line-through';
-  }
-});
-
-// Function to remove items
-function removeItems() {
-  const itemsToRemove = document.querySelectorAll('li');
-  for (let index = 0; index < itemsToRemove.length; index += 1) {
-    itemsToRemove[index].remove(li);
+// 8-  Função para remover a classe selecionado
+function limpaSelecao() {
+  const selecao = document.querySelectorAll('li');
+  for (let index = 0; index < selecao.length; index += 1) {
+    selecao[index].classList.remove('selecionado');
   }
 }
 
-// Button to remove all li
-// if (confirm('Você tem certeza que deseja apagar todas as tarefas?')) {
-  buttonRemoveAll.addEventListener('click', function () {
-    if (document.querySelectorAll('li').length > 0) {
-      removeItems('li');
+// 7- Função realiza a ação de adicionar e retirar a seleção com 'click'
+function seleciona() {
+  const selecionar = document.querySelector('#lista-tarefas');
+  selecionar.addEventListener('click', function (event) {
+    const selecao = event.target;
+    let cont = 0;
+    for (let index = 0; index < selecao.classList.length; index += 1) {
+      if (selecao.classList[index] === 'selecionado') {
+        cont += 1;
+      }
+    }
+    if (cont === 0) {
+      limpaSelecao();
+      selecao.classList.add('selecionado');
+    }/*  else {
+      limpaSelecao();
+    } */
+  });
+}
+
+// 13- Adiciona evento para subir a posição do item selecionado na lista
+function botaoSobe() {
+  const lista = document.querySelector('#lista-tarefas');
+  const botao = document.querySelector('#mover-cima');
+  botao.addEventListener('click', function () {
+    const tarefa = document.querySelector('.selecionado');
+    if (tarefa) {
+      if (tarefa.previousElementSibling !== null) {
+        lista.insertBefore(tarefa, tarefa.previousElementSibling);
+      }
     }
   });
-// }
+}
 
-// Button to remove a selected li
-buttonRemoveSelected.addEventListener('click', function () {
-  if (document.querySelectorAll('.selected').length > 0) {
-    removeItems('.selected');
-  }
-});
+// 13- Adiciona evento para descer a posição do item selecionado na lista
+function botaoDesce() {
+  const lista = document.querySelector('#lista-tarefas');
+  const botao = document.querySelector('#mover-baixo');
+  botao.addEventListener('click', function () {
+    const tarefa = document.querySelector('.selecionado');
+    if (tarefa) {
+      if (tarefa.nextElementSibling !== null) {
+        lista.insertBefore(tarefa.nextElementSibling, tarefa);
+      }
+    }
+  });
+}
 
-// Button to remove all completed li
-buttonRemoveAllCompleted.addEventListener('click', function () {
-  if (document.querySelectorAll('.completed').length > 0) {
-    removeItems('.completed');
+// 9- Adiciona evento de duplo 'click' para indicar tarefa concluída com 'tachado'
+function tarefaConcluida() {
+  const listaTarefas = document.querySelector('#lista-tarefas');
+  listaTarefas.addEventListener('dblclick', function (event) {
+    const completado = event.target;
+    let cont = 0;
+    for (let index = 0; index < completado.classList.length; index += 1) {
+      if (completado.classList[index] === 'completed') {
+        cont += 1;
+      }
+    }
+    if (cont === 0) {
+      completado.classList.add('completed');
+    } else {
+      completado.classList.remove('completed');
+    }
+  });
+}
+
+// 10- Adiciona evento para limpar todo conteúdo da lista
+function ApagaTudo() {
+  const botao = document.querySelector('#apaga-tudo');
+  botao.addEventListener('click', function () {
+    const lista = document.getElementById('lista-tarefas');
+    while (lista.hasChildNodes()) {
+      lista.removeChild(lista.firstChild);
+    }
+  });
+}
+
+// 11- Adiciona evento para remover as tarefas concluídas da lista
+function apagaConcluido() {
+  const botao = document.querySelector('#remover-finalizados');
+  botao.addEventListener('click', function () {
+    const tarefa = document.querySelectorAll('.completed');
+    for (let index = 0; index < tarefa.length; index += 1) {
+      tarefa[index].remove();
+    }
+  });
+}
+
+// 12- Armazena os dados da lista no 'Web Storage' do 'browser'
+function webStorage() {
+  const botao = document.querySelector('#salvar-tarefas');
+  botao.addEventListener('click', function () {
+    const lista = document.getElementsByTagName('li');
+    if (typeof (Storage) !== 'undefined') {
+      const tarefas = [];
+      for (let index = 0; index < lista.length; index += 1) {
+        tarefas.push({ value: lista[index].innerHTML, class: lista[index].className });
+      }
+      localStorage.setItem('tarefasSalvas', JSON.stringify(tarefas));
+    }
+  });
+}
+
+// 12- Recupera os dados do 'Web Storage' ao recarregar a página
+function recuperarSalvos() {
+  const lista = document.getElementById('lista-tarefas');
+  let salvos = {
+    class: '',
+    value: '',
+  };
+  if (localStorage.length !== 0) {
+    salvos = JSON.parse(localStorage.getItem('tarefasSalvas'));
+    for (let index = 0; index < salvos.length; index += 1) {
+      const recupera = document.createElement('li');
+      recupera.innerText = salvos[index].value;
+      recupera.className = salvos[index].class;
+      lista.appendChild(recupera);
+    }
   }
-});
+}
+
+// 14- Adiciona evento para remover os itens selecionados
+function removerSelecionado() {
+  const botao = document.querySelector('#remover-selecionado');
+  botao.addEventListener('click', function () {
+    const tarefa = document.querySelector('.selecionado');
+    if (tarefa) {
+      tarefa.remove();
+    }
+  });
+}
+
+// Adiciona um círculo à página
+function criarCirculo() {
+  const main = document.querySelector('main');
+  const circulo = document.createElement('div');
+  circulo.id = 'circulo';
+  circulo.classList.add('circulo');
+  main.appendChild(circulo);
+}
+
+// Adiciona um elemento 'footer' à página
+function criarRodape() {
+  const corpo = document.querySelector('#body-content');
+  const rodape = document.createElement('footer');
+  rodape.id = 'rodape';
+  rodape.classList.add('rodape');
+  rodape.innerText = '</> por Leonardo Majevski - 2020 ©️';
+  rodape.setAttribute('href', 'www.teste.com.br');
+  corpo.appendChild(rodape);
+}
+
+// Chama as funções após o carregamento HTML
+window.onload = function () {
+  criarBotao('Adicionar 📝', 'criar-tarefa', 'tarefas');
+  criarBotao('⤴️', 'mover-cima', 'controles');
+  criarBotao('⤵️', 'mover-baixo', 'controles');
+  criarBotao('Salvar 💾', 'salvar-tarefas', 'controles');
+  criarBotao('Selecionado ❌', 'remover-selecionado', 'controles');
+  criarBotao('Concluídos ❌', 'remover-finalizados', 'controles');
+  criarBotao('Limpar Lista 🗑', 'apaga-tudo', 'controles');
+  criarCirculo();
+  funcaoAdicionar();
+  enterAdicionar();
+  seleciona();
+  tarefaConcluida();
+  webStorage();
+  recuperarSalvos();
+  apagaConcluido();
+  ApagaTudo();
+  botaoSobe();
+  botaoDesce();
+  removerSelecionado();
+};
